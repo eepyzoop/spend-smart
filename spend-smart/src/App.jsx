4e0aa744ec9a26240e09d08694e633584c875e7e
eepyzoop <l240577@lhr.nu.edu.pk>
2026-06-19 02:48:02 +0500

    initial commit project setup

---
+++ spend-smart/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-8">
+      <h1 className="text-3xl font-bold">SpendSmart is alive!</h1>
+    </div>
+  )
+}
+
+export default App
